7918b785105a58adef5d8d383b0dda2bbd7841bc
maxxies <ahiamadzormaxwell7@gmail.com>
2023-05-16 20:44:57 +0000

    Add Page modal component

---
+++ src/general_components/page_modal/page_modal.test.tsx
@@ -0,0 +1,1 @@
+//Test for Page Modal component
---
+++ src/general_components/page_modal/page_modal.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './page_modal.module.css'
+
+function PageModal(){
+    return (
+    <>
+        <section className={styles.container}>
+            <div className={styles.modal}>
+
+            </div>
+        </section>
+    </>
+    )
+}
+
+export default PageModal;
