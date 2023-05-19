297ead3c85a61a8e91bb0693f3acee0d67405f23
maxxies <ahiamadzormaxwell7@gmail.com>
2023-05-19 04:34:53 +0000

    Add props and logic to navigation buttons

---
+++ src/general_components/navigation_button/navigation_button.tsx
@@ -0,0 +1,34 @@
+import styles from "./navigation_button.module.css";
+import { NavigationButtonProps } from "./navigation_button.props.ts";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+import { HiOutlineArrowNarrowLeft } from "react-icons/hi";
+
+function NavButton(props: NavigationButtonProps) {
+	return (
+		<>
+			{props.type === "next" ? (
+				<button
+					className={`${styles.button} ${styles.next}`}
+					data-testid="nav-button"
+					onClick={props.onClick}>
+					<span className={styles.text}>Next</span>
+					<i>
+						<HiOutlineArrowNarrowRight />
+					</i>
+				</button>
+			) : (
+				<button
+					className={`${styles.button} ${styles.prev}`}
+					data-testid="nav-button"
+					onClick={props.onClick}>
+					<i>
+						<HiOutlineArrowNarrowLeft />
+					</i>
+					<span className={styles.text}>Previous</span>
+				</button>
+			)}
+		</>
+	);
+}
+
+export default NavButton;
