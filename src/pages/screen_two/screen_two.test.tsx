ec5de71330105a89604db0a473b0c6d316b7a1c8
maxxies <ahiamadzormaxwell7@gmail.com>
2023-06-24 00:12:59 +0000

    Modify emoton toolkit

--- src/pages/screen_two/screen_two.test.tsx
+++ src/pages/screen_two/screen_two.test.tsx
@@ -1,7 +1,6 @@
 import { describe, test, expect } from "vitest";
 import { render } from "@testing-library/react";
 import ScreenTwo from "./screen_two";
-import TextEditor from "./components/text_editor/text_editor";
 import store from "../../store/store";
 import { Provider } from "react-redux";
 
@@ -14,20 +13,6 @@ describe("Screen Two", () => {
             </Provider>
         );
         const component = getByTestId("screen-two");
-
-        expect(component).toBeInTheDocument();
-    });
-});
-
-// checks if Page Modal component is mounted
-describe("Text Editor", () => {
-    test("should render the component", () => {
-        const { getByTestId } = render(
-            <Provider store={store}>
-                <TextEditor changeButton={() => {}} />
-            </Provider>
-        );
-        const component = getByTestId("text-editor");
 
         expect(component).toBeInTheDocument();
     });
